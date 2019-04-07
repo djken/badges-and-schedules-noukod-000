4c873cf2884b7c319d2b620fe848eff85f637380
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-07 02:47:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr_names)
+  new_arr = []
+  arr_names.each do |n|
+    return "Hello, my name is #{new_arr[n]}."
+  end
+end
+
+arr_name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+batch_badge_creator(arr_name)
+
+def assign_rooms(speakers)
+  
+end
+
+def printer
+  
+end
